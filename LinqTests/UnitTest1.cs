0a1284c06a4d5c92bd7467c90f6c395ed7c03e3e
Js-Yang <chyang590@gmail.com>
2018-03-24 10:15:41 +0800

    use linq

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -3,6 +3,7 @@ using ExpectedObjects;
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTests
 {
@@ -23,6 +24,22 @@ namespace LinqTests
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void find_products_that_price_between_200_and_500_2()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = WithoutLinq.FindProductByPrice(products, 200, 500);
+
+            var expected = new List<Product>()
+            {
+                new Product{Id=2, Cost=21, Price=210, Supplier="Yahoo" },
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
+            };
+            
+            expected.ToExpectedObject().ShouldEqual(actual.Where(x=>x.Price < 500 && x.Price >200).ToList());
         }
     }
 }
