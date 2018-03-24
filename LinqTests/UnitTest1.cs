eb22812880e5b9f84fe0346bf5b45240f9bca655
Js-Yang <chyang590@gmail.com>
2018-03-24 10:08:06 +0800

    first version

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using ExpectedObjects;
+﻿using System.Collections;
+using ExpectedObjects;
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
@@ -16,7 +17,9 @@ namespace LinqTests
 
             var expected = new List<Product>()
             {
-                //todo
+                new Product{Id=2, Cost=21, Price=210, Supplier="Yahoo" },
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
             };
 
             expected.ToExpectedObject().ShouldEqual(actual);
@@ -28,7 +31,16 @@ internal class WithoutLinq
 {
     public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary)
     {
-        throw new System.NotImplementedException();
+        var result = new List<Product>();
+        
+        foreach (var product in products)
+        {
+            if (product.Price > lowBoundary && product.Price < highBoundary)
+            {
+                result.Add(product);
+            }   
+        }
+        return result;
     }
 }
 
