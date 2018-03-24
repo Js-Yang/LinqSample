3e6599599de606d907b6a0a63cac20206ebe060e
Js-Yang <chyang590@gmail.com>
2018-03-24 11:14:28 +0800

    change to use static method

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using ExpectedObjects;
 using LinqTests;
@@ -13,7 +14,7 @@ namespace LinqTests
         public void find_products_that_price_between_200_and_500_cost_higher_then30()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = WithoutLinq.FindProductByPrice(products, 200, 500);
+            var actual = products.FindProducts(product => product.Price > 200 && product.Price < 500 && product.Cost > 30);
 
             var expected = new List<Product>()
             {
@@ -21,18 +22,17 @@ namespace LinqTests
                 new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
         [TestMethod]
         public void find_products_that_price_between_200_and_500_2()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = products.Where(x => x.Price < 500 && x.Price > 200);
+            var actual = products.Where(x => x.Price < 500 && x.Price > 200 && x.Cost > 30);
 
             var expected = new List<Product>()
             {
-                new Product{Id=2, Cost=21, Price=210, Supplier="Yahoo" },
                 new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
                 new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
             };
@@ -42,20 +42,17 @@ namespace LinqTests
     }
 }
 
-internal class WithoutLinq
+internal static class WithoutLinq
 {
-    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary)
+    public static IEnumerable<Product> FindProducts(this IEnumerable<Product> products, Func<Product, bool> predicate)
     {
-        var result = new List<Product>();
-
         foreach (var product in products)
         {
-            if (product.Price > lowBoundary && product.Price < highBoundary && product.Cost > 30)
+            if (predicate(product))
             {
-                result.Add(product);
+                yield return product;
             }
         }
-        return result;
     }
 }
 
